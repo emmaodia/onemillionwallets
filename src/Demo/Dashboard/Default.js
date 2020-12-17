bd28465ee104c9eb70aeda5811272262214be7a0
emmaodia <odia.emma@gmail.com>
2020-12-17 16:14:17 +0100

    Removed unsed declared variables.

--- src/Demo/Dashboard/Default.js
+++ src/Demo/Dashboard/Default.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import {Row, Col, Card, Table, Tabs, Tab} from 'react-bootstrap';
+import {Row, Col, Card } from 'react-bootstrap';
 
 import Aux from "../../hoc/_Aux";
-import DEMO from "../../store/constant";
-
-import avatar1 from '../../assets/images/user/avatar-1.jpg';
-import avatar2 from '../../assets/images/user/avatar-2.jpg';
-import avatar3 from '../../assets/images/user/avatar-3.jpg';
-
-import Axios from 'axios';
 
 const Dashboard = () => {
 
