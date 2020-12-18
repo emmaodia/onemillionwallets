b52475bf1dab99ed677615fe2c1393a526516d73
emmaodia <odia.emma@gmail.com>
2020-12-18 14:05:05 +0100

    Feature: Find Data on parsed strings.

--- src/Demo/Dashboard/Balances.js
+++ src/Demo/Dashboard/Balances.js
@@ -2,21 +2,43 @@ import React, { useState, useEffect } from 'react';
 import {Row, Col, Card, InputGroup, FormControl} from 'react-bootstrap';
 import Balances from '../../App/components/Balances'
 import Aux from "../../hoc/_Aux";
-import ContentLoader from "react-content-loader";
+// import ContentLoader from "react-content-loader";
 
 import Axios from 'axios';
 
 const Dashboard = () => {
 
     const [balances, setBalances] = useState([]);
-    const [fetching, setFetching] = useState(true);
-
-    const url = `https://api.covalenthq.com/v1/1/address/0x32D9b5C41d594838d6b993ebAF538fF770a00E30/balances_v2/?key=API_KEY`
+    const [walletAddress, setWalletAddress] = useState('');
+    const [ queryWallet, setQueryWallet ] = useState('')
+    // const [fetching, setFetching] = useState(true);
 
     
+    const getWalletAdd = e => {
+        setQueryWallet(e.target.value)
+        console.log(queryWallet)
+    }
+
+    const searchResult = e => {
+        e.preventDefault()
+        setWalletAddress(queryWallet)
+        
+        // setWalletAddress('')
+        console.log(walletAddress)
+        console.log('here3')
+    }
 
     useEffect(() => {
-        const getTicker = async () => {
+
+        let url = `https://api.covalenthq.com/v1/1/address/${walletAddress}/balances_v2/?key=API_KEY`
+
+
+        const getTransfer = async (value) => {
+
+        // if ( value != null && value !== '') {
+        //         value = `0x32d9b5c41d594838d6b993ebaf538ff770a00e30`
+        //     }   
+
             const response = await Axios ({
             url: url,
             method: "GET"
@@ -26,51 +48,52 @@ const Dashboard = () => {
         console.log('here')
         console.log(data)
         setBalances(data)
-        setFetching(false);
+        // setFetching(false);
         }
         
-        getTicker();
-    }, [setBalances, url])
+        getTransfer(walletAddress);
+    }, [setBalances, walletAddress])
 
 
-    if (fetching) return  <Card.Body>
-                            <Row>
-                                <Col md={12}>   <h5>Data Loading...</h5> 
-                                    <ContentLoader 
-                                        speed={2}
-                                        width={'75%'}
-                                        height={160}
-                                        viewBox="0 0 400 160"
-                                        backgroundColor="#f3f3f3"
-                                        foregroundColor="#ecebeb"
+    // if (fetching) return  <Card.Body>
+    //                         <Row>
+    //                             <Col md={12}>   <h5>Data Loading...</h5> 
+    //                                 <ContentLoader 
+    //                                     speed={2}
+    //                                     width={'75%'}
+    //                                     height={160}
+    //                                     viewBox="0 0 400 160"
+    //                                     backgroundColor="#f3f3f3"
+    //                                     foregroundColor="#ecebeb"
                                         
-                                    >
-                                    <rect x="80" y="40" rx="3" ry="3" width="400" height="100" /> 
-                                    <rect x="80" y="40" rx="4" ry="4" width="400" height="100" />
-                                    <rect x="80" y="40" rx="3" ry="3" width="400" height="100" />
-                                    </ContentLoader>
-                                </Col>
-                            </Row>
-                        </Card.Body>
+    //                                 >
+    //                                 <rect x="80" y="40" rx="3" ry="3" width="400" height="100" /> 
+    //                                 <rect x="80" y="40" rx="4" ry="4" width="400" height="100" />
+    //                                 <rect x="80" y="40" rx="3" ry="3" width="400" height="100" />
+    //                                 </ContentLoader>
+    //                             </Col>
+    //                         </Row>
+    //                     </Card.Body>
 
         return (
             <Aux>
                 <Card>
                     <Card.Header>
-                    <Card.Title as="h6">Enter a Wallet address to view all Transactions</Card.Title>
+                    <Card.Title as="h6">Enter a Wallet address to view Balances</Card.Title>
                     </Card.Header>
                     <Card.Body>
                     <Row>
                         <Col md={12}>
                         <InputGroup className="mb-4">
                             <InputGroup.Prepend>
-                            <InputGroup.Text>Search</InputGroup.Text>
+                            <InputGroup.Text onClick={searchResult}>Search</InputGroup.Text>
                             </InputGroup.Prepend>
-                            <FormControl as="textarea" aria-label="With textarea" />
+                            <FormControl aria-label="With textarea" value={queryWallet} onChange={getWalletAdd} />
                         </InputGroup>
                         </Col>
                     </Row>
                     </Card.Body>
+        {/* <div>{walletAddress}</div> */}
                 </Card>
                 <Row>
                     
