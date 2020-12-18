4d71bc6955ea4c18c2287bf5bb6c7d43fa383fd2
emmaodia <odia.emma@gmail.com>
2020-12-18 14:04:13 +0100

    Defined Transfers routes

--- src/routes.js
+++ src/routes.js
@@ -9,12 +9,14 @@ const DashboardDefault = React.lazy(() => import('./Demo/Dashboard/Default'));
 const DashboardTickers = React.lazy(() => import('./Demo/Dashboard/Tickers'));
 const DashboardBalances = React.lazy(() => import('./Demo/Dashboard/Balances'));
 const DashboardTransactions = React.lazy(() => import('./Demo/Dashboard/Transactions')); 
+const DashboardTransfers = React.lazy(() => import('./Demo/Dashboard/Transfers')); 
 
 const routes = [
     { path: '/dashboard/default', exact: true, name: 'Default', component: DashboardDefault },
     { path: '/dashboard/tickers', exact: true, name: 'Tickers', component: DashboardTickers },
     { path: '/dashboard/balances', exact: true, name: 'Balances', component: DashboardBalances },
     { path: '/dashboard/transactions', exact: true, name: 'Transactions', component: DashboardTransactions },
+    { path: '/dashboard/transfers', exact: true, name: 'Transfers', component: DashboardTransfers },
 ];
 
 export default routes;
